641efff68fdf77fa4f2314318d5bc887eeea7ec0
Levi Sørum <levi.sorum@dnb.no>
2025-02-24 14:19:28 +0100

    Registration form support

--- app/components/sanity/accordion.tsx
+++ app/components/sanity/accordion.tsx
@@ -8,9 +8,11 @@ export default function Accordion({
   children: ReactNode | ReactNode[] | string;
 }) {
   return (
-    <details>
-      <summary>{summary}</summary>
-      {children}
+    <details className="flex flex-col gap-4 mt-4 mb-2">
+      <summary className="text-base font-medium">{summary}</summary>
+      <div className="prose mx-1 [&>p]:text-sm [&>p]:font-normal">
+        {children}
+      </div>
     </details>
   );
 }
--- app/components/sanity/registrationForm/index.tsx
+++ app/components/sanity/registrationForm/index.tsx
@@ -1,7 +1,7 @@
 export default function RegistrationForm({ src }: { src: string }) {
   return (
     <iframe
-      className="flex flex-col justify-center min-h-[900px] sm:min-h-[770px] lg:min-h-[740px]"
+      className="flex flex-col justify-center -mx-2 min-h-[900px] sm:min-h-[770px] lg:min-h-[740px]"
       allowFullScreen
       lang="no"
       src={src}
